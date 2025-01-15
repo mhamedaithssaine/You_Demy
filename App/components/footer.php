9f2a5373105d17f82a241172e5648609c421f28f
Mhamedaithssaine <mhamedaithssaine1@gmail.com>
2025-01-15 19:22:00 +0100

    ajouter footer components

---
+++ App/components/footer.php
@@ -0,0 +1,18 @@
+<footer class="bg-white border-t border-gray-200 p-4">
+    <div class="flex items-center justify-between">
+        <div>
+            <p class="text-sm text-gray-600">© 2023 Youdemy. All rights reserved.</p>
+        </div>
+        <div class="flex items-center gap-4">
+            <a href="#" class="text-gray-600 hover:text-gray-900">
+                <i class="fab fa-facebook-f w-5 h-5"></i>
+            </a>
+            <a href="#" class="text-gray-600 hover:text-gray-900">
+                <i class="fab fa-twitter w-5 h-5"></i>
+            </a>
+            <a href="#" class="text-gray-600 hover:text-gray-900">
+                <i class="fab fa-instagram w-5 h-5"></i>
+            </a>
+        </div>
+    </div>
+</footer>
