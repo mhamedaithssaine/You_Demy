e58988de0d93a841ff5169f14561646c82f252fb
Mhamedaithssaine <mhamedaithssaine1@gmail.com>
2025-01-17 19:52:53 +0100

    ajouter deux methode activateUser et suspendedUser pour la gestion des utilisateur qui fait par l'admin

--- App/models/User.php
+++ App/models/User.php
@@ -1,16 +1,43 @@
 <?php
 namespace App\Models;
+
 use App\Models\Crud;
 use PDO;
-abstract class User extends Crud {
+ class User extends Crud {
+ 
+   private $table = 'users';
+
+   public function __construct(){
+       parent::__construct();
+   }
+
+   public function selectAllusers(){
+       return $this->selectRecords($this->table);
+   }
+
+   public function selectusers($id){
+       return $this->selectRecords($this->table, '*', 'id = ' . $id);
+   }
+
+   public function addusers(array $data){
+       return $this->insertRecord($this->table, $data);
+   }
+
+   public function updateusers( array $data, int $id){
+       return $this->updateRecord($this->table, $data, $id);
+   }
+
+   public function deleteusers(int $id){
+       return $this->deleteRecord($this->table, $id);
+   }
+
+   public function activateUser($id){
+    return $this->updateRecord($this->table,['status'=>'active'],$id);
+   }
+   public function suspendUser($id){
+    return $this->updateRecord($this->table, ['status' => 'suspended'], $id);
+   }
    
-public function __construct(PDO $pdo){
-parent::__construct($pdo,'users');
-}
-
-abstract public function getRole();
-
-}
-
+ }
 
 ?>
