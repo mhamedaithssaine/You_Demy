a944e5614f3c1684c749548f5544114c19649c2a
Mhamedaithssaine <mhamedaithssaine1@gmail.com>
2025-01-19 20:30:11 +0100

    Créer ce fichier pour ajouter logique qui permet de supprimer l'inscription de cours

---
+++ App/Etudiant/delet_inscription.php
@@ -0,0 +1,26 @@
+<?php
+require '../../vendor/autoload.php';
+
+use App\Models\Cours;
+
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    header('Location: ../../login.php');
+    exit;
+}
+
+$student = new Cours();
+
+if (isset($_POST['cours_id'])) {
+    $coursId = $_POST['cours_id'];
+    $etudiantId = $_SESSION['user_id']; 
+
+    if ($student->deleteInscription($etudiantId, $coursId)) {
+        header('Location: ../../etudiant.php?success=Inscription  avec succès');
+    } else {
+        header('Location: ../../etudiant.php?error=Erreur lors de l inscription');
+    }
+} else {
+    header('Location: ../../etudiant.php');
+}
