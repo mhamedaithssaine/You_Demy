bdc9219fd4f2ce1b120137a904ded56f33aaf5be
Mhamedaithssaine <mhamedaithssaine1@gmail.com>
2025-01-19 20:34:40 +0100

    modifier l'appel de la methode par ce que j'ai le deplace vers class admin

--- App/admin/manage_enseignants.php
+++ App/admin/manage_enseignants.php
@@ -1,21 +1,23 @@
 <?php
 require '../../vendor/autoload.php';
-use App\Models\Enseignant;
 
-$enseignant = new Enseignant();
+use App\Models\Admin;
+
+
+$admin = new Admin();
 
 if (isset($_GET['action']) && isset($_GET['id'])) {
     $action = $_GET['action'];
-    $enseignantId = intval($_GET['id']); 
+    $id = intval($_GET['id']); 
   
     if ($action === 'validate') {
-        $enseignant->validateEnseignant($enseignantId);
+        $admin->validateEnseignant($id);
     } elseif ($action === 'reject') {
-        $enseignant->rejectEnseignant($enseignantId);
+        $admin->rejectEnseignant($id);
     }
-    header("Location: ../index.php");
+    header("Location: ../../index.php");
     exit();
 }
 
-$pendingEnseignants = $enseignant->getPendingEnseignants();
+
 ?>
