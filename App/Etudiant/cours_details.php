b693a5ef1c22407868e4108626aae475d8af1601
Mhamedaithssaine <mhamedaithssaine1@gmail.com>
2025-01-21 00:57:40 +0100

    modifier l'affichage de details de cours et les coloure des button, et adapter les path

--- App/Etudiant/cours_details.php
+++ App/Etudiant/cours_details.php
@@ -58,7 +58,7 @@ $tags = $coursModel->getCourseTags($coursId);
         </div>
         <div class="flex items-center">
             <form method="POST" action="../components/logout.php" style="display:inline;">
-                <button type="submit" class="bg-red-500 text-white px-4 py-2 rounded">Déconnexion</button>
+                <button type="submit" class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800">Logout</button>
             </form>
         </div>
     </div>
