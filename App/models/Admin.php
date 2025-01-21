bccdc00ee734df54e43971ee4d436ddaf758528b
Mhamedaithssaine <mhamedaithssaine1@gmail.com>
2025-01-21 23:19:37 +0100

    ajouter une methode recuperer les statistuqe de cours enseignant ect

--- App/models/Admin.php
+++ App/models/Admin.php
@@ -2,6 +2,7 @@
 namespace App\Models;
 
 use App\Models\User;
+use PDO;
 
 class Admin extends User {
     public function __construct() {
@@ -27,6 +28,26 @@ class Admin extends User {
     public function rejectEnseignant(int $id) {
         return $this->updateRecord($this->table, ['role' => 'etudiant' ,'valide' => 'valide'], $id);
     }
+
+    public function getGeneralStats() {
+        $query = "
+            SELECT
+                (SELECT COUNT(*) FROM users WHERE role = 'enseignant' AND status = 'active') AS total_teachers,
+                (SELECT COUNT(*) FROM users WHERE role = 'etudiant' AND status = 'active') AS total_students,
+                (SELECT COUNT(*) FROM cours WHERE status = 'published') AS active_courses,
+                (SELECT COUNT(*) FROM cours WHERE status = 'draft') AS pending_courses,
+                (SELECT COUNT(*) FROM categories) AS total_categories
+        ";
+
+        $stmt = self::$conn->prepare($query);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    
+   
+    
+    
 
 }
 ?>
